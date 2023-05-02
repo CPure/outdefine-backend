3869dd2e61356cb7fe6ff6fa2242e4aa3e2aae91
Cpure <carlospures@gmail.com>
2023-05-02 00:45:23 -0500

    type def

--- src/routes/spellcheck.js
+++ src/routes/spellcheck.js
@@ -2,9 +2,15 @@ const router = require('express-promise-router')()
 const spellcheckController = require('../controllers/spellcheck')
 
 /**
+ * @typedef Result
+ * @property {Array} suggestions
+ * @property {boolean} correct
+ */
+
+/**
  * GET /api/spellcheck
  * @tags spellcheck
-
+ * @returns {Result}
  */
 router.get('/:word', spellcheckController.getSpellcheckWord)
 
